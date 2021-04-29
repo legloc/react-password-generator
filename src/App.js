1bf912d56c11cfcc95f26fbea0e4e9bd3f23d05f
legloc <aliiev.akhtem@gmail.com>
2021-04-29 12:58:09 +0300

    Initial commit. Created template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.sass'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h4 className="card-title fw-bold text-center">Password Generator</h4>
+
+              <div className="mb-3">
+                <input className="form-control pointer" type="text" value="CLICK GENERATE" disabled />
+                <small className="d-block text-muted text-center">Click to copy</small>
+              </div>
+              
+              <div className="range-wrapper mb-2">
+                <span>Length: <span className="fw-bold">32</span></span>
+                <div className="d-flex align-items-center">
+                  <span>4</span>
+                  <input className="form-range mx-2" type="range" id="customRange" min="4" max="32" />
+                  <span>32</span>
+                </div>
+              </div>
+
+              <div className="option-wrapper mb-2">
+                <span>Include Uppercase</span>
+                <div className="form-check form-switch">
+                  <input className="form-check-input pointer" type="checkbox" />
+                </div>
+              </div>
+
+              <div className="option-wrapper mb-2">
+                <span>Include Lowercase</span>
+                <div className="form-check form-switch">
+                  <input className="form-check-input pointer" type="checkbox" />
+                </div>
+              </div>
+
+              <div className="option-wrapper mb-2">
+                <span>Include Symbols</span>
+                <div className="form-check form-switch">
+                  <input className="form-check-input pointer" type="checkbox" />
+                </div>
+              </div>
+
+              <div className="option-wrapper mb-2">
+                <span>Include Numbers</span>
+                <div className="form-check form-switch">
+                  <input className="form-check-input pointer" type="checkbox" />
+                </div>
+              </div>
+
+              <button className="btn w-100 btn-primary">GENERATE PASSWORD</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
